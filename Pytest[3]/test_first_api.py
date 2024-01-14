2db0e532d08b4ac5d769287863abf409cd2c15b6
psy_qa <ravehazard1@gmail.com>
2024-01-14 22:39:07 +0300

    edit first test (parametrize)

--- Pytest[3]/test_first_api.py
+++ Pytest[3]/test_first_api.py
@@ -1,11 +1,20 @@
 import requests
-
+import pytest
 
 class TestFirstAPI:
 
-    def test_hello_call(self):
+    names = [
+        ("Vitalik"),
+        ("Arseniy"),
+        ("Konstantin"),
+        ("Valera Zhmishenko"),
+        ("")
+    ]
+
+
+    @pytest.mark.parametrize('name', names)
+    def test_hello_call(self, name):
         url = 'https://playground.learnqa.ru/api/hello'
-        name = 'Valera Zhmishenko'
         data = {"name": name}
         response = requests.get(url=url, params=data)
 
@@ -15,7 +24,10 @@ class TestFirstAPI:
 
         assert 'answer' in response_dict, "There is no field 'answer' in the response"
 
-        expected_response_text = f"Hello, {name}"
+        if len(name) == 0:
+            expected_response_text = 'Hello, someone'
+        else:
+            expected_response_text = f"Hello, {name}"
         actual_response_text = response_dict.get('answer')
 
         assert expected_response_text == actual_response_text, "Actual text in the response is not correct"
