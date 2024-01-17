d909ab00b89528f8290606e6fa04d14697c5547b
psy_qa <ravehazard1@gmail.com>
2024-01-17 23:17:14 +0300

    edit test_user_register.py

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,4 +1,4 @@
-import requests
+from lib.my_requests import MyRequests
 from lib.BaseCase import BaseCase
 from lib.assertions import Assertions
 
@@ -9,8 +9,8 @@ class TestUserRegister(BaseCase):
 
     def test_create_user_succefuly(self):
         data = self.prepare_registration_data()
-        response = requests.post(url='https://playground.learnqa.ru/api/user', data=data)
-
+        # response = requests.post(url='https://playground.learnqa.ru/api/user', data=data)
+        response = MyRequests.post(url='/user', data=data)
         Assertions.assert_code_status(response, 200)
         Assertions.assert_json_has_key(response, 'id')
 
@@ -18,7 +18,8 @@ class TestUserRegister(BaseCase):
     def test_create_user_with_existing_email(self):
         email = 'qqq123test@mail.ru'
         data = self.prepare_registration_data(email)
-        response = requests.post(url='https://playground.learnqa.ru/api/user', data=data)
+        # response = requests.post(url='https://playground.learnqa.ru/api/user', data=data)
+        response = MyRequests.post(url='/user', data=data)
 
         Assertions.assert_code_status(response, 400)
         assert response.content.decode(
