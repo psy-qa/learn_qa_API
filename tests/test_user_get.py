0caf1a09eb690e397f0615fa9b82e65b2a7174dd
psy_qa <ravehazard1@gmail.com>
2024-01-17 23:15:57 +0300

    edit test_user_get.py

--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -1,12 +1,13 @@
-import requests
 from lib.BaseCase import BaseCase
 from lib.assertions import Assertions
+from lib.my_requests import MyRequests
 
 
 class TestUserGet(BaseCase):
 
     def test_get_user_details_not_auth(self):
-        response = requests.get('https://playground.learnqa.ru/api/user/2')
+        # response = requests.get('https://playground.learnqa.ru/api/user/2')
+        response = MyRequests.get('/user/2')
         Assertions.assert_json_has_key(response, 'username')
         Assertions.assert_json_has_not_key(response, 'email')
         Assertions.assert_json_has_not_key(response, 'firstName')
@@ -17,17 +18,20 @@ class TestUserGet(BaseCase):
             'email': 'vinkotov@example.com',
             'password': '1234'
         }
-        response_1 = requests.post(url='https://playground.learnqa.ru/api/user/login', data=data)
+        # response_1 = requests.post(url='https://playground.learnqa.ru/api/user/login', data=data)
+        response_1 = MyRequests.post(url='/user/login', data=data)
 
         auth_sid = self.get_cookie(response_1, 'auth_sid')
         token = self.get_header(response_1, 'x-csrf-token')
         user_id_from_auth_method = self.get_json_value(response_1, "user_id")
         Assertions.assert_json_has_key(response_1, 'user_id')
 
-        response_2 = requests.get(f'https://playground.learnqa.ru/api/user/{user_id_from_auth_method}',
+        # response_2 = requests.get(f'https://playground.learnqa.ru/api/user/{user_id_from_auth_method}',
+        #                           headers={'x-csrf-token': token},
+        #                           cookies={'auth_sid': auth_sid})
+        response_2 = MyRequests.get(f'/user/{user_id_from_auth_method}',
                                   headers={'x-csrf-token': token},
                                   cookies={'auth_sid': auth_sid})
-
         # Assertions.assert_json_has_key(response_2, 'username')
         # Assertions.assert_json_has_key(response_2, 'firstName')
         # Assertions.assert_json_has_key(response_2, 'lastName')
